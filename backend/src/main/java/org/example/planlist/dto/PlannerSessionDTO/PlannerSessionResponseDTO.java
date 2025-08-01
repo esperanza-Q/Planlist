f708e8e639cfec8679e63f67f5910a2c7cdb9510
eunhee <kxnhy4227@gmail.com>
2025-08-01 21:35:30 +0900

    Fix/#6

--- backend/src/main/java/org/example/planlist/dto/PlannerSessionDTO/PlannerSessionResponseDTO.java
+++ backend/src/main/java/org/example/planlist/dto/PlannerSessionDTO/PlannerSessionResponseDTO.java
@@ -13,8 +13,8 @@ import java.time.LocalTime;
 @SuperBuilder
 public abstract class PlannerSessionResponseDTO {
 
-    private Long plannerId;
-    private Long projectId;
+//    private Long plannerId;
+//    private Long projectId;
     private String title;
     private Integer week;
     private LocalDate date;
