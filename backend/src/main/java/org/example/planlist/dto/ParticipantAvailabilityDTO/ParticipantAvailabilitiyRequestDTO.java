7f2ea0e81a1f8699bc4848653abb2a32d88771fc
sujin <20241005@sungshin.ac.kr>
2025-08-02 04:40:50 +0900

    Fix:ParticipantAvailabilityEntity 어노테이션 추가(#3)

--- backend/src/main/java/org/example/planlist/dto/ParticipantAvailabilityDTO/ParticipantAvailabilitiyRequestDTO.java
+++ backend/src/main/java/org/example/planlist/dto/ParticipantAvailabilityDTO/ParticipantAvailabilitiyRequestDTO.java
@@ -1,5 +1,8 @@
 package org.example.planlist.dto.ParticipantAvailabilityDTO;
 
+import jakarta.validation.constraints.NotNull;
+
 public class ParticipantAvailabilitiyRequestDTO {
+    @NotNull
     private Long projectId;
 }
