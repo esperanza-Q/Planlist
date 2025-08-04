0168e5000e63546de08ef3609b82e0afe3ed4702
sujin <20241005@sungshin.ac.kr>
2025-08-04 23:43:39 +0900

    Feat:MoveBetweenPlaces Entity 추가(#16)

--- backend/src/main/java/org/example/planlist/entity/MoveBetweenPlaces.java
+++ backend/src/main/java/org/example/planlist/entity/MoveBetweenPlaces.java
@@ -1,4 +1,39 @@
 package org.example.planlist.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Builder
+@AllArgsConstructor
+@Table(name="move_between_places")
 public class MoveBetweenPlaces {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "move_id", nullable = false)
+    private Long moveId;
+
+    @Column(name = "transportation", nullable = false)
+    private String transportation;
+
+    @Column(name = "duration_min", nullable = false)
+    private Long durationMin;
+
+    @Column(name = "travel_date", nullable = false)
+    private LocalDate travelDate;
+
+    @ManyToOne
+    @JoinColumn(name = "calendar_id")
+    private ParticipantAvailability availability;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id")
+    private PlannerProject project;
 }
