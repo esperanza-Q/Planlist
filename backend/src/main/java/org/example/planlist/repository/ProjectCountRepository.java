5a5636941ed191d1a63baa628a111299afa0372a
Son <suruna1026@gmail.com>
2025-08-09 04:10:55 +0900

    feat : addFreetimeCalendar, show, home

--- backend/src/main/java/org/example/planlist/repository/ProjectCountRepository.java
+++ backend/src/main/java/org/example/planlist/repository/ProjectCountRepository.java
@@ -1,11 +1,12 @@
 package org.example.planlist.repository;
 
 import org.example.planlist.entity.ProjectCount;
+import org.example.planlist.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface ProjectCountRepository extends JpaRepository<ProjectCount, Long> {
     Optional<ProjectCount> findByProjectCountId(Long projectCountId);
+    Optional<ProjectCount> findByUser(User user);
 }
-
