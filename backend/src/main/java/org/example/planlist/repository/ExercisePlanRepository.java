0247ddb1366427a51e8f8333517cc16f65395722
Son <suruna1026@gmail.com>
2025-08-12 04:29:53 +0900

    feat : sessionPage

--- backend/src/main/java/org/example/planlist/repository/ExercisePlanRepository.java
+++ backend/src/main/java/org/example/planlist/repository/ExercisePlanRepository.java
@@ -1,10 +1,14 @@
 package org.example.planlist.repository;
 
 import org.example.planlist.entity.ExercisePlan;
+import org.example.planlist.entity.PtSession;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ExercisePlanRepository extends JpaRepository<ExercisePlan, Long> {
     Optional<ExercisePlan> findByExercisePlanId(Long exercisePlanId);
+
+    List<ExercisePlan> findByPlanner(PtSession planner);
 }
